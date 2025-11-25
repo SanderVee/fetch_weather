0b8fb83c71a249280561b6fc346b8291460c27e3
Sander Vassel <sander.vassel@gmail.com>
2025-11-25 22:18:16 +0200

    .py lisäys

---
+++ fetch_weather.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import mysql.connector
+from datetime import datetime
+
+API_KEY = 
+CITY = 'Helsinki'
+URL = 
+
+conn = mysql.connector.connect(
+    host='localhost',
+    user='ubuntu',
+    password=
+    database=
+)
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS weather_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    city VARCHAR(50),
+    temperature FLOAT,
+    description VARCHAR(100),
+    timestamp DATETIME
+)''')
+
+response = requests.get(URL)
+data = response.json()
+temp = data['main']['temp']
+desc = data['weather'][0]['description']
+timestamp = datetime.now()
+
+cursor.execute('INSERT INTO weather_data (city, temperature, description, timestamp) VALUES (%s, %s, %s, %s)',
+               (CITY, temp, desc, timestamp))
+conn.commit()
+cursor.close()
+conn.close()
+
+print(f'Data tallennettu: {CITY} {temp}°C {desc}')
